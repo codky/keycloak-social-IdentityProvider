1a548a980e0af3c8f855b0637d150b16bfd325a8
codky <msky0912@gmail.com>
2022-11-18 18:25:29 +0900

    fix Kakao Identity Provider

--- src/main/java/com/nexgrid/keycloaksocialnaver/KakaoIdentityProvider.java
+++ src/main/java/com/nexgrid/keycloaksocialnaver/KakaoIdentityProvider.java
@@ -1,6 +1,7 @@
 package com.nexgrid.keycloaksocialnaver;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.broker.oidc.AbstractOAuth2IdentityProvider;
 import org.keycloak.broker.oidc.OAuth2IdentityProviderConfig;
 import org.keycloak.broker.oidc.mappers.AbstractJsonUserAttributeMapper;
@@ -10,13 +11,17 @@ import org.keycloak.broker.provider.util.SimpleHttp;
 import org.keycloak.broker.social.SocialIdentityProvider;
 import org.keycloak.events.EventBuilder;
 import org.keycloak.models.KeycloakSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class KakaoIdentityProvider extends AbstractOAuth2IdentityProvider implements SocialIdentityProvider {
+
+    private final static Logger log = LoggerFactory.getLogger(KakaoIdentityProvider.class);
 
     public static final String AUTH_URL = "https://kauth.kakao.com/oauth/authorize";
     public static final String TOKEN_URL = "https://kauth.kakao.com/oauth/token";
     public static final String PROFILE_URL = "https://kapi.kakao.com/v2/user/me";
-    public static final String DEFAULT_SCOPE = "basic";
+    public static final String DEFAULT_SCOPE = "account_email,openid,profile_nickname";
 
     public KakaoIdentityProvider(KeycloakSession session, OAuth2IdentityProviderConfig config) {
         super(session, config);
@@ -37,13 +42,17 @@ public class KakaoIdentityProvider extends AbstractOAuth2IdentityProvider implem
 
     @Override
     protected BrokeredIdentityContext extractIdentityFromProfile(EventBuilder event, JsonNode profile) {
-        BrokeredIdentityContext user = new BrokeredIdentityContext(profile.get("response").get("id").asText());
 
-        String email = profile.get("response").get("email").asText();
+        log.info("extractIdentityFromProfile.profile = " + profile);
+        long longtypevalue = profile.get("id").asLong();
+        String id = String.valueOf(longtypevalue);
+        BrokeredIdentityContext user = new BrokeredIdentityContext(id);
+        log.info("extractIdentityFromProfile.user = " + user);
+
+        String nickname = profile.get("properties").get("nickname").asText();
 
         user.setIdpConfig(getConfig());
-        user.setUsername(email);
-        user.setEmail(email);
+        user.setUsername(nickname);
         user.setIdp(this);
 
         AbstractJsonUserAttributeMapper.storeUserProfileForMapper(user, profile, getConfig().getAlias());
@@ -55,17 +64,19 @@ public class KakaoIdentityProvider extends AbstractOAuth2IdentityProvider implem
     protected BrokeredIdentityContext doGetFederatedIdentity(String accessToken) {
         try {
             JsonNode profile = SimpleHttp.doGet(PROFILE_URL, session).param("access_token", accessToken).asJson();
+            log.info("doGetFederatedIdentity.profile = " + profile);
 
             BrokeredIdentityContext user = extractIdentityFromProfile(null, profile);
+            log.info("doGetFederatedIdentity.user = " + user);
 
             return user;
         } catch (Exception e) {
-            throw new IdentityBrokerException("Could not obtain user profile from naver.", e);
+            throw new IdentityBrokerException("Could not obtain user profile from kakao.", e);
         }
     }
 
     @Override
     protected String getDefaultScopes() {
-        return "";
+        return DEFAULT_SCOPE;
     }
 }
